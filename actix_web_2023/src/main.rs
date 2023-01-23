4021c1985b639727aba7c5ee04a109e92f591a9c
kimjooyoon <asketeddy@gmail.com>
2023-01-23 10:23:52 +0900

    test: 테스트 분리 및 test name 추가

--- actix_web_2023/src/main.rs
+++ actix_web_2023/src/main.rs
@@ -30,23 +30,42 @@ async fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use actix_web::{body::to_bytes, dev::Service, http, test, web, App, Error};
+    use actix_http::body::BoxBody;
+    use actix_web::{body::to_bytes, dev::Service, http, test, App, Error};
+    use actix_web::dev::ServiceResponse;
 
     use super::*;
 
     #[actix_web::test]
     async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let app = test::init_service(app).await;
-
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await?;
-
+        let url = String::from("/");
+        let resp = test_request(url).await?;
         assert_eq!(resp.status(), http::StatusCode::OK);
 
         let response_body = resp.into_body();
         assert_eq!(to_bytes(response_body).await?, r##"hi teddy!"##);
 
         Ok(())
+    }
+
+    #[actix_web::test]
+    async fn test_name() -> Result<(), Error> {
+        let url = String::from("/name_test");
+        let resp = test_request(url).await?;
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = resp.into_body();
+        assert_eq!(to_bytes(response_body).await?, "Hello name_test!");
+
+        Ok(())
+    }
+
+    async fn test_request(url : String)
+        -> Result<ServiceResponse<BoxBody>, Error> {
+        let app = App::new().service(index).service(name);
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri(&url).to_request();
+        app.call(req).await
     }
 }
