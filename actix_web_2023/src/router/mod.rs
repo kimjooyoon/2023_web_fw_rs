0a205cd9512917bae507ec42be090a23acf11793
kimjooyoon <asketeddy@gmail.com>
2023-01-23 12:59:53 +0900

    feat(actix): get_user() service 등록
    Closes #15

--- actix_web_2023/src/router/mod.rs
+++ actix_web_2023/src/router/mod.rs
@@ -1,5 +1,6 @@
 use actix_web::{App, HttpServer, middleware, web};
 use crate::handler::{index, name};
+use crate::user::handler::get_user;
 
 pub async fn app_run() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
@@ -10,6 +11,7 @@ pub async fn app_run() -> std::io::Result<()> {
             .service(web::resource("/index.html").to(|| async { "hi teddy!" }))
             .service(name)
             .service(index)
+            .service(get_user)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
